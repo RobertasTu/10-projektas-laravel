a7b24414d19a2eee5b1e05ed50ca1ff430552acc
RobertasTu <tumelis.robertas@gmail.com>
2021-11-04 14:20:21 +0200

    Update 2021_10_29_102415_create_tasks_table.php

--- database/migrations/2021_10_29_102415_create_tasks_table.php
+++ database/migrations/2021_10_29_102415_create_tasks_table.php
@@ -16,7 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->Longtext('description');
+            $table->longText('description');
             $table->unsignedBigInteger('type_id');
             $table->unsignedBigInteger('owner_id');
             $table->dateTime('start_date');
